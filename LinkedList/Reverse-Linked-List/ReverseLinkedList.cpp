04477d896ad73974a1bca6c82065d6ea209f1820
sdyan <31694573+yansudong@users.noreply.github.com>
2021-02-27 10:57:27 +0800

    Update ReverseLinkedList.cpp

--- LinkedList/Reverse-Linked-List/ReverseLinkedList.cpp
+++ LinkedList/Reverse-Linked-List/ReverseLinkedList.cpp
@@ -1,5 +1,27 @@
-/*  # 206 Reverse Linked List
-*   # 2021-2-27
-*
-*
-*/
+/**
+ * #206 Reverse Linked List
+ * 【2021-2-27】
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+// 迭代法
+class Solution1 {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+        while(curr) {
+            ListNode* next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+};
