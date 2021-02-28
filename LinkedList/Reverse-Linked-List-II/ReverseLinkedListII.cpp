12a4ae43eb92c355da6b731b5f03c12d53cc6280
sdyan <31694573+yansudong@users.noreply.github.com>
2021-02-28 18:56:36 +0800

    Update ReverseLinkedListII.cpp

--- LinkedList/Reverse-Linked-List-II/ReverseLinkedListII.cpp
+++ LinkedList/Reverse-Linked-List-II/ReverseLinkedListII.cpp
@@ -1,3 +1,42 @@
 /**
-* #92 Reverse Linked List II
-*
+ * #92 Reverse Linked List II
+ * 【2021-2-28】
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* successor = NULL; // 后驱节点
+
+    // 反转以 head 为起点的 n 个节点，返回新的头结点
+    ListNode* reverseN(ListNode* head, int n) {
+        if (n == 1) { 
+            // 记录第 n + 1 个节点
+            successor = head->next;
+            return head;
+        }
+        // 以 head.next 为起点，需要反转前 n - 1 个节点
+        ListNode* last = reverseN(head->next, n - 1);
+
+        head->next->next = head;
+        // 让反转之后的 head 节点和后面的节点连起来
+        head->next = successor;
+        return last;
+    }
+
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+        // base case
+        if (left == 1) {
+            return reverseN(head, right);
+        }
+        // 前进到反转的起点触发 base case
+        head->next = reverseBetween(head->next, left - 1, right - 1);
+        return head;
+    }
+};
