8a911c91d530544aed3a9dafd498adf03760e655
sdyan <31694573+yansudong@users.noreply.github.com>
2021-03-05 19:11:45 +0800

    Update LinkedListCycle.cpp

--- LinkedList/Linked-List-Cycle/LinkedListCycle.cpp
+++ LinkedList/Linked-List-Cycle/LinkedListCycle.cpp
@@ -1,4 +1,48 @@
 /**
  * #141 Linked List Cycle
- *
- *
+ * 【2021-3-5】
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+
+// 方法一 哈希表
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        unordered_set<ListNode*> seen;
+        while (head != nullptr) {
+            if (seen.count(head)) {
+                return true;
+            }
+            seen.insert(head);
+            head = head->next;
+        }
+        return false;
+    }
+};
+
+
+//方法二 快慢指针
+class Solution {
+public:
+    bool hasCycle(ListNode* head) {
+        if (head == nullptr || head->next == nullptr) {
+            return false;
+        }
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+        while (slow != fast) {
+            if (fast == nullptr || fast->next == nullptr) {
+                return false;
+            }
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return true;
+    }
+};
